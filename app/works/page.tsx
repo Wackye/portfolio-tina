583db31e85f37a72666bde080ddb147e0183b42e
weizhe <j4170149@gmail.com>
2024-10-30 23:53:17 +0800

    Remove code impacted ESLint error.

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,17 +1,11 @@
 import Layout from "../../components/layout/layout";
-import client from "../../tina/__generated__/client";
 import ClientPage from "./client-page";
 
 export default async function WorksPage() {
-  const posts = await client.queries.postConnection();
-
-  if (!posts) {
-    return null;
-  }
 
   return (
     <Layout>
-      <ClientPage posts={posts} />
+      <ClientPage />
     </Layout>
   );
 }
